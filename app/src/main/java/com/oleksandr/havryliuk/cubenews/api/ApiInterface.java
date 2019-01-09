c57ce1cdc12eab8810fa96b4b55bfdde445fad5d
Oleksandr Havryliuk <sashagavgavgav@gmail.com>
2019-01-09 23:02:35 +0200

    add: ApiClient and ApiInterface

---
+++ app/src/main/java/com/oleksandr/havryliuk/cubenews/api/ApiInterface.java
@@ -0,0 +1,16 @@
+package com.oleksandr.havryliuk.cubenews.api;
+
+import com.oleksandr.havryliuk.cubenews.models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<News> getNews(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+}
