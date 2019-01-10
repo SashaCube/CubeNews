4e3a0ca811e8df418953c7ae8c6e2677707597ff
Oleksandr Havryliuk <sashagavgavgav@gmail.com>
2019-01-10 10:58:24 +0200

    set recycler view in MainActivity, loadJson

--- app/src/main/java/com/oleksandr/havryliuk/cubenews/MainActivity.java
+++ app/src/main/java/com/oleksandr/havryliuk/cubenews/MainActivity.java
@@ -2,12 +2,78 @@ package com.oleksandr.havryliuk.cubenews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.oleksandr.havryliuk.cubenews.api.ApiClient;
+import com.oleksandr.havryliuk.cubenews.api.ApiInterface;
+import com.oleksandr.havryliuk.cubenews.models.Article;
+import com.oleksandr.havryliuk.cubenews.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public  static final String TAG = MainActivity.class.getName();
+    public static final String API_KEY = "c9d28729e28d43cfadefbd0f4675f4ae";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerVeiw);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        loadJson();
+    }
+
+    public void loadJson(){
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country, API_KEY);
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful() && response.body().getArticles() != null){
+
+                    if(!articles.isEmpty()){
+                        articles.clear();
+                    }
+
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(articles, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }else{
+                    Toast.makeText(MainActivity.this, "No Result!", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+
     }
 }
